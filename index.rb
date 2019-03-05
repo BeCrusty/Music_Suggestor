9d4aea37fec163ac6579357899554625b614363a
Michael Bahari <bahayana@gmail.com>
2019-03-05 14:55:39 +1100

    fixed syntax, restructuring music data

--- index.rb
+++ index.rb
@@ -1,65 +1,100 @@
-# Create a music list
-music_list = { 1 => "Happy by Pharell Williams", 2 => "Random song", 3 => "You Are the Sunshine of My Life by Stevie Wonder"}
+# Create a list of songs for each category
+
+on_top = ["Happy", "Pharrell Williams", "https://www.youtube.com/watch?v=ZbZSe6N_BXs"]
+
+#create a list of each category for Mood & Occasion
+mood_cat = [on_top, blue, in_love, sleep, rage, high]
+occasion_cat = [netflix, dance, board_game, cosplay, fancy_dinner, hangin_garret]
+#create a hash that contains Mood & Occasion lists
+music_list = { :mood_cat => mood_cat, :occasion_cat => occasion_cat}
+
+
+# Create an array of moods
 moods = [
-    "1. Happy", "2. Melancholy", "3. Romantic", 
-    "4. Energetic", "5. Sleepy", "6. Raging"
+    "1. On top of the world", 
+    "2. Blue", 
+    "3. In love", 
+    "4. Sleep deprived", 
+    "5. Rage", 
+    "6. A little high"
 ]
+# Create an array of occasions
 occasions = [
-    "1. Alone Time", "2. Dance Battle", "3. Board Game Night", 
-    "4. BBQ with Garret", "5. Fancy Dinner Party", "6. Cosplay Party"
+    "1. Netflix & Chill", 
+    "2. Dance Battle", 
+    "3. Fantasy Board Game Night", 
+    "4. Cosplay Party", 
+    "5. Fancy Dinner Party", 
+    "6. Hangin' Out With Garret"
 ]
 # Output a welcome and ask for the users name
 puts "Welcome to Ben & Mike's Music Suggestor"
-puts "What can I call you?"
+puts "What can we call you by?"
 name = gets.chomp
-running = true
-puts "Welcome #{name}! Are we choosing music for a Mood, or for an Occasion? Input 1 for Mood or 2 for Occasion"
+
 # Greet the user using their input name and present the first choice between mood & moment
+puts "Hello #{name}! Would you like to select a song for a Mood or an Occasion? Input 1 for Mood or 2 for Occasion"
+
+# Opening a while loop to ensure correct input for first choice
+# true = continue to run the loop, false = exit the loop
+running = true
 while running
     puts "1. Mood \n2. Occasion "
     choice = gets.to_i
+
+    # Correct input will make the first or second condition true, if input is incorrect loop will begin again
+    # true = continue to run the loop, false = exit the loop
     choosing = true
-    # Diverge based on the user's choice and offer the next choice between the 6 categories
     if choice == 1
-        puts "What kind of mood are you in right now?"
+        puts "Are you feeling:"
+
+        # Opening a while loop for moods to ensure correct input
         while choosing
             moods.each do |item| puts item end
             next_choice = gets.to_i
+
+            # if statement ensures input is within parameters
             if next_choice > 0 and next_choice <= moods.length
-                next_choice
+                #because hashes start at 0, need a -1
+                next_choice -= 1
+                song_suggestion = music_list[:mood_cat][next_choice]
                 choosing = false
             else
+                # error message
                 puts "Sorry, that input didn't seem right. We need the number of your selection."
                 choosing = true
             end
         end
         running = false
     elsif choice == 2
-        puts "What are you doing right now?"
+        puts "What's the occasion?"
+
+        # Opening a while loop for occasions to ensure correct input
         while choosing
-            options.each do |item| puts item end
+            occasions.each do |item| puts item end
             next_choice = gets.to_i
-            if next_choice > 0 and next_choice <= moods.length
+
+            # if statement ensures input is within parameters
+            if next_choice > 0 and next_choice <= occasions.length
                 next_choice
                 choosing = false
             else
+                # error message
                 puts "Sorry, that input didn't seem right. We need the number of your selection."
                 choosing = true
             end
         end
         running = false
     else
+        # error message
         puts "Sorry, that input didn't seem right. We need the number of your selection."
         running = true
-        # loop back around
+        # loops back to Mood or Occasion
     end
 end
 
-song_suggestion = music_list[next_choice]
-puts "We suggest you listen to: '#{song_suggestion}'"
+puts "Ben & Mike suggest: '#{song_suggestion}'"
 
 # Output their choice and generate a song selection from a stored list
-
 # Output details about the song and a link to listen to it
-
 # Ask if the user would like another song and loop back to the first choice, else exit
