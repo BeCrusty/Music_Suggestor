5c5d9db08741f58e1e2f9741f08bfc764f71384f
Michael Bahari <bahayana@gmail.com>
2019-03-05 15:44:30 +1100

    finished basic database, simplified control flows

--- index.rb
+++ index.rb
@@ -1,6 +1,18 @@
 # Create a list of songs for each category
 
 on_top = ["Happy", "Pharrell Williams", "https://www.youtube.com/watch?v=ZbZSe6N_BXs"]
+blue = ["Blue (Da Ba Dee)", "Eiffel 65", "https://www.youtube.com/watch?v=68ugkg9RePc"]
+in_love = ["You Are The Sunshine Of My Life", "Stevie Wonder", "https://www.youtube.com/watch?v=3wZ_b_uUAdQ"]
+sleep = ["A Whole New World", "John McClung", "https://www.youtube.com/watch?v=-GN997853gc"]
+rage = ["Kamikaze", "Eminem", "https://www.youtube.com/watch?v=FhF9RwkHAJw"]
+high = ["Three Little Birds", "Bob Marley", "https://www.youtube.com/watch?v=LanCLS_hIo4"]  
+
+netflix = ["Can't Get Enough Of Your Love Baby", "Barry White", "https://www.youtube.com/watch?v=x0I6mhZ5wMw"]
+dance = ["It's Tricky", "Run-DMC", "https://www.youtube.com/watch?v=l-O5IHVhWj0"]
+board_game = ["Concerning Hobbits", "Howard Shore", "https://www.youtube.com/watch?v=0gLd7rpBJlY"]
+cosplay = ["Sailor Moon Theme", "Nicole and Brynne Price", "https://www.youtube.com/watch?v=5txHGxJRwtQ"]  
+fancy_dinner = ["Impromptu in G flat major D.899, Op. 90 - III. Andante mosso", "Franz Schubert", "https://www.youtube.com/watch?v=Nt33K-HsOHA"]
+hangin_garret = ["Southern Nights", "Glen Campbell", "https://www.youtube.com/watch?v=7wOUFo4Lwf8"]
 
 #create a list of each category for Mood & Occasion
 mood_cat = [
@@ -19,9 +31,6 @@ occasion_cat = [
     fancy_dinner, 
     hangin_garret
 ]
-#create a hash that contains Mood & Occasion lists
-music_list = { :mood_cat => mood_cat, :occasion_cat => occasion_cat}
-
 
 # Create an array of moods
 moods = [
@@ -42,7 +51,7 @@ occasions = [
     "6. Hangin' Out With Garret"
 ]
 # Output a welcome and ask for the users name
-puts "Welcome to Ben & Mike's Music Suggestor"
+puts "Welcome to Ben & Mike's Music Suggestor!"
 puts "What can we call you by?"
 name = gets.chomp
 
@@ -51,6 +60,9 @@ puts "Hello #{name}! Would you like to select a song for a Mood or an Occasion? 
 
 # Opening a while loop to ensure correct input for first choice
 # true = continue to run the loop, false = exit the loop
+# input 1 opens Mood categories in the if statement
+# input 2 open Occasion categories in the elsif statement
+# else statement catches errors
 running = true
 while running
     puts "1. Mood \n2. Occasion "
@@ -60,7 +72,7 @@ while running
     # true = continue to run the loop, false = exit the loop
     choosing = true
     if choice == 1
-        puts "Are you feeling:"
+        puts "Are you feeling:"     
 
         # Opening a while loop for moods to ensure correct input
         while choosing
@@ -71,7 +83,7 @@ while running
             if next_choice > 0 and next_choice <= moods.length
                 #because hashes start at 0, need a -1
                 next_choice -= 1
-                song_suggestion = music_list[:mood_cat][next_choice]
+                song_suggestion = mood_cat[next_choice]
                 choosing = false
             else
                 # error message
@@ -90,7 +102,8 @@ while running
 
             # if statement ensures input is within parameters
             if next_choice > 0 and next_choice <= occasions.length
-                next_choice
+                next_choice -= 1
+                song_suggestion = occasion_cat[next_choice]
                 choosing = false
             else
                 # error message
@@ -107,7 +120,15 @@ while running
     end
 end
 
-puts "Ben & Mike suggest: '#{song_suggestion}'"
+song_name = song_suggestion[0]
+song_artist = song_suggestion[1]
+song_link = song_suggestion[2]
+
+
+
+puts "Ben & Mike suggest: #{song_name} by #{song_artist}. You can listen to it here: #{song_link}"
+
+
 
 # Output their choice and generate a song selection from a stored list
 # Output details about the song and a link to listen to it
